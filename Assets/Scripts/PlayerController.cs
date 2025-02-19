5f42764b68e1211e673b1836a273f601d9a08718
kenel003 <53184959+kenel003@users.noreply.github.com>
2025-02-19 14:58:48 -0600

    Wed

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,11 +2,32 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+/* TODO:
+ * 1.) Projectiles
+ * 2.) Sound
+ * 3.) Enemies/Stuff to shoot
+ * 4.) Moving asteroids in random direction
+ * 5.) Spawn asteroids randomly
+ * 6.) Add hyperspace
+ * 7.) Fleet carriers
+ * 8.) Cockpit for ship and fleet carrier
+ * 9.) More ships to choose from
+ * 10.) Stars
+ * 11.) Stations
+ * 12.) Upgrades for ship
+ * 13.) Exit ship and control human player
+ * 14.) SRV (rover vehicle thingamajig)
+ 
+  
+ */
+
 public class PlayerController : MonoBehaviour
 {
     public Rigidbody rb;
     public float speed = 10000f, forwardControl, verticalControl, horizontalControl, mouseSensitivity = 2.0f;
     public Vector2 mouseChange, mouseDirection, shipDirection;
+    public AudioSource engineSoundSource;
+    public AudioClip engineSoundClip;
 
     private void Start()
     {
@@ -27,8 +48,17 @@ public class PlayerController : MonoBehaviour
         float mouseUp = mouseSensitivity * Input.GetAxis("Mouse Y");
         float mouseSide = mouseSensitivity * Input.GetAxis("Mouse X");
         //transform.Rotate(-mouseUp, mouseSide, 0);
-        rb.AddTorque(-mouseUp, mouseSide, 0);
+        rb.AddRelativeTorque(-mouseUp, mouseSide, 0, ForceMode.VelocityChange);
 
+        engineSoundSource.volume = Mathf.Clamp( (Mathf.Abs(forwardControl) + 
+                                    Mathf.Abs(verticalControl) +
+                                    Mathf.Abs(horizontalControl)
+                                    ), 0f, 1f);
+
+        /*if (forwardControl != 0 || verticalControl != 0 || horizontalControl != 0)
+        {
+            engineSoundSource.PlayOneShot(engineSoundClip);
+        }*/
 
     }
 
